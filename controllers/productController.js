afc71a21bbd6dc087f40a8d8ce2c5402ea237011
Luke Glendining <lukeglendining@gmail.com>
2021-07-22 23:57:51 +0200

    I added the product filtering and sorting as well as pagination features to my API.

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,11 +1,62 @@
 const Products = require("../models/productModel");
 
+// Filter, sorting and pagination
+class APIfeatures {
+  constructor(query, queryString) {
+    this.query = query;
+    this.queryString = queryString;
+  }
+  filtering() {
+    const queryObj = { ...this.queryString };
+
+    const excludedFields = ["page", "sort", "limit"];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(
+      /\b(gte|gt|lt|lte|regex)\b/g,
+      (match) => "$" + match
+    );
+
+    this.query.find(JSON.parse(queryStr));
+
+    return this;
+  }
+  sorting() {
+    if (this.queryString.sort) {
+      const sortBy = this.queryString.sort.split(",").join("");
+      console.log(sortBy);
+      this.query = this.query.sort(sortBy);
+    } else {
+      this.query = this.query.sort("-createdAt");
+    }
+    return this;
+  }
+  paginating() {
+    const page = this.queryString.page * 1 || 1;
+    const limit = this.queryString.limt * 1 || 9;
+    const skip = (page - 1) * limit;
+    this.query = this.query.skip(skip).limit(limit);
+    return this;
+  }
+}
+
 const productController = {
   getProducts: async (req, res) => {
     try {
-      const products = await Products.find();
+      console.log(req.query);
+      const features = new APIfeatures(Products.find(), req.query)
+        .filtering()
+        .sorting()
+        .paginating();
 
-      res.json(products);
+      const products = await features.query;
+
+      res.json({
+        status: "success",
+        result: products.length,
+        products: products,
+      });
     } catch (err) {
       return res.status(500).json({ msg: err.message });
     }
