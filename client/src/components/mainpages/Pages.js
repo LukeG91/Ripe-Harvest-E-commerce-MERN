42f70434d2fe6db68d3c3e08e0ee550b7c002dda
Luke Glendining <lukeglendining@gmail.com>
2021-07-23 08:38:32 +0200

    I added auth(Register & Login) components, as well as the products and cart components.

--- client/src/components/mainpages/Pages.js
+++ client/src/components/mainpages/Pages.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Products from "./products/Products";
+import Login from "./auth/Login";
+import Register from "./auth/Register";
+import Cart from "./cart/Cart";
+import NotFound from "./utils/not_found/NotFound";
 
 function Pages() {
-  return <div>Main Pages Component</div>;
+  return (
+    <Switch>
+      <Route path="/" exact component={Products} />
+      <Route path="/login" exact component={Login} />
+      <Route path="/register" exact component={Register} />
+      <Route path="/cart" exact component={Cart} />
+
+      {/* For any route that is not part of the above. */}
+      <Route path="*" exact component={NotFound} />
+    </Switch>
+  );
 }
 
 export default Pages;
