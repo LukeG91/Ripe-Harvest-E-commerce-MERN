c62a8a3ddaba6736a1b316606fafbc056d27ee4b
Luke Glendining <lukeglendining@gmail.com>
2021-08-16 14:08:36 +0200

    Added the functionality to allow the user to sprt products according to their category.

--- client/src/components/mainpages/products/Products.js
+++ client/src/components/mainpages/products/Products.js
@@ -3,6 +3,7 @@ import { GlobalState } from "../../../GlobalState";
 import ProductItem from "../utils/productItem/ProductItem";
 import Loading from "../utils/loading/Loading";
 import axios from "axios";
+import Filters from "./Filters";
 
 function Products() {
   const state = useContext(GlobalState);
@@ -11,36 +12,69 @@ function Products() {
   const [token] = state.token;
   const [callback, setCallback] = state.productsAPI.callback;
   const [loading, setLoading] = useState(false);
+  const [isCheck, setIsCheck] = useState(false);
 
   const handleCheck = (id) => {
-    console.log(id);
+    products.forEach((product) => {
+      if (product._id === id) product.checked = !product.checked;
+    });
+    setProducts([...products]);
   };
 
   const deleteProduct = async (id, public_id) => {
-    console.log({ id, public_id });
-    // try {
-    //   setLoading(true);
-    //   const destroyImg = await axios.post(
-    //     "/api/destroy",
-    //     { public_id },
-    //     {
-    //       headers: { Authorization: token },
-    //     }
-    //   );
-    // const deleteProduct = await axios.delete(`/api/products/${id}`, {
-    //   headers: { Authorization: token },
-    // });
-    //   await destroyImg;
-    //   await deleteProduct;
-    //   setLoading(false);
-    //   setCallback(!callback);
-    // } catch (error) {
-    //   alert(error.response.data.msg);
-    // }
+    // console.log({ id, public_id });
+    try {
+      setLoading(true);
+      const destroyImg = await axios.post(
+        "/api/destroy",
+        { public_id },
+        {
+          headers: { Authorization: token },
+        }
+      );
+      const deleteProduct = await axios.delete(`/api/products/${id}`, {
+        headers: { Authorization: token },
+      });
+      await destroyImg;
+      await deleteProduct;
+      setCallback(!callback);
+      setLoading(false);
+    } catch (error) {
+      alert(error.response.data.msg);
+    }
   };
+
+  const checkAll = () => {
+    products.forEach((product) => {
+      product.checked = !isCheck;
+    });
+    setProducts([...products]);
+    setIsCheck(!isCheck);
+  };
+
+  const deleteAll = () => {
+    products.forEach((product) => {
+      if (product.checked) deleteProduct(product._id, product.images.public_id);
+    });
+  };
+
+  if (loading)
+    return (
+      <div>
+        <Loading />
+      </div>
+    );
 
   return (
     <>
+      <Filters />
+      {isAdmin && (
+        <div className="delete-all">
+          <span>Select all</span>
+          <input type="checkbox" checked={isCheck} onChange={checkAll} />
+          <button onClick={deleteAll}>Delete ALL</button>
+        </div>
+      )}
       <div className="products">
         {products.map((product) => {
           return (
