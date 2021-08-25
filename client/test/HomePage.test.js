de13f70c632968f5ebbc46f0cd360c03d2d20ad3
Luke Glendining <lukeglendining@gmail.com>
2021-08-25 15:48:35 +0200

    Added snapshot testing.

---
+++ client/test/HomePage.test.js
@@ -0,0 +1,9 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import HomePage from "../src/components/Home/Home";
+
+/* Creating a snapshot test to check that the Home page renders correctly */
+it("I am running a snapshot test in order to ensure that the Home page component renders correctly.", () => {
+  const output = renderer.create(<HomePage />).toJSON();
+  expect(output).toMatchSnapshot();
+});
